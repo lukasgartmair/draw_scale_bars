f729d3c7835d2193c4b0298dc67193305d500560
lukasgartmair <lukas_gartmair@gmx.de>
2016-12-18 14:38:56 +0100

    added pointsize as argument

--- draw_scalebar.sh
+++ draw_scalebar.sh
@@ -8,6 +8,12 @@
 image_name=$1
 scale_label=$2
 unit=$3
+point_size=$4
+
+
+border_scale_factor=0.7
+text_scale_factor=1.6
+
 
 echo "$image_name"
 
@@ -21,8 +27,12 @@ height_border=`convert -format "%[fx:h*0.15]" $image_name info:`
 lsx=`convert -format "%[fx:20*w/100]" $image_name info:`
 lex=`convert -format "%[fx:80*w/100]"  $image_name info:`
 
-ly=`convert -format "%[fx:((100*h)/100) + ($height_border*0.7)]"  $image_name info:`
-ytext=`convert -format "%[fx:($ly - $height)/1.6 ]"  $image_name info:`
+
+# 0.7 for border_scale_factor is a valid value
+# 1.6 for text sclae factor
+
+ly=`convert -format "%[fx:((100*h)/100) + ($height_border*$border_scale_factor)]"  $image_name info:`
+ytext=`convert -format "%[fx:($ly - $height)/$text_scale_factor ]"  $image_name info:`
 
 echo "$height_border"
 echo "$width"
@@ -34,4 +44,4 @@ echo "$ytext"
 
 # add a bar at the bottom
 convert -background 'graya(85%, 0.5)'  -gravity south -splice 0x$height_border $image_name $image'_with_border.eps'
-convert -stroke black -strokewidth 2 -pointsize 25 -gravity south -draw "line $lsx,$ly $lex,$ly  text 0,$ytext '$scale_label $unit'" $image'_with_border.eps' $image'_with_scalebar.eps'
+convert -stroke black -strokewidth 2 -pointsize $point_size -gravity south -draw "line $lsx,$ly $lex,$ly  text 0,$ytext '$scale_label $unit'" $image'_with_border.eps' $image'_with_scalebar.eps'
